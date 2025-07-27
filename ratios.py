2c53790f8684ecd5c9a2239ea4245e59396ee1cd
ishaanjajodia <153287021+ishaanjajodia@users.noreply.github.com>
2025-07-27 14:28:44 +0530

    Update ratios.py

--- ratios.py
+++ ratios.py
@@ -62,3 +62,80 @@ def calculate_altman_z_scores(data):
         results.append({'Year': year, 'Altman Z-Score': round(Z, 2), 'Risk Zone': risk})
 
     return results
+
+
+
+# ratios.py
+
+import json
+
+def safe_div(a, b):
+    return a / b if b else 0
+
+def calculate_beneish_m_score(data):
+    annuals = [x for x in data['financials'] if x['Type'] == 'Annual']
+    annuals = sorted(annuals, key=lambda x: int(x['FiscalYear']))
+
+    results = []
+    # Initialize arrays
+    years, revenue, gross_profit, accounts_receivable, inventory = [], [], [], [], []
+    current_assets, ppe, total_assets, depreciation, sgna = [], [], [], [], []
+    sales, current_liabilities, long_term_debt, net_income, cfo, non_op_income = [], [], [], [], [], []
+
+    for entry in annuals:
+        years.append(entry['FiscalYear'])
+        inc = entry['stockFinancialMap']['INC']
+        revenue.append(float(next((v['value'] for v in inc if v['key'] == 'TotalRevenue'), 0)))
+        gross_profit.append(float(next((v['value'] for v in inc if v['key'] == 'GrossProfit'), 0)))
+        depreciation.append(float(next((v['value'] for v in inc if v['key'] == 'Depreciation/Amortization'), 0)))
+        sgna.append(float(next((v['value'] for v in inc if v['key'] == 'Selling/General/AdminExpensesTotal'), 0)))
+        net_income.append(float(next((v['value'] for v in inc if v['key'] == 'NetIncome'), 0)))
+        non_op_income.append(float(next((v['value'] for v in inc if v['key'] == 'OtherNet'), 0)))
+        cas = entry['stockFinancialMap']['CAS']
+        cfo.append(float(next((v['value'] for v in cas if v['key'] == 'CashfromOperatingActivities'), 0)))
+        bal = entry['stockFinancialMap']['BAL']
+        accounts_receivable.append(float(next((v['value'] for v in bal if v['key'] == "AccountsReceivable-TradeNet"), 0)))
+        inventory.append(float(next((v['value'] for v in bal if v['key'] == "TotalInventory"), 0)))
+        current_assets.append(float(next((v['value'] for v in bal if v['key'] == "TotalCurrentAssets"), 0)))
+        ppe.append(float(next((v['value'] for v in bal if v['key'] == "Property/Plant/EquipmentTotal-Net"), 0)))
+        total_assets.append(float(next((v['value'] for v in bal if v['key'] == "TotalAssets"), 0)))
+        current_liabilities.append(float(next((v['value'] for v in bal if v['key'] == "TotalCurrentLiabilities"), 0)))
+        long_term_debt.append(float(next((v['value'] for v in bal if v['key'] == "LongTermDebt"), 0)))
+        sales.append(float(next((v['value'] for v in inc if v['key'] == 'Revenue'), 0)))
+
+    for i in range(1, len(years)):
+        dsri = safe_div(accounts_receivable[i] / revenue[i], accounts_receivable[i-1] / revenue[i-1])
+        gmi = safe_div((gross_profit[i-1] / revenue[i-1]), (gross_profit[i] / revenue[i]))
+        aqi = safe_div(1 - (current_assets[i] + ppe[i]) / total_assets[i], 1 - (current_assets[i-1] + ppe[i-1]) / total_assets[i-1])
+        sgi = safe_div(sales[i], sales[i-1])
+        depi = safe_div(depreciation[i-1] / (depreciation[i-1] + ppe[i-1]), depreciation[i] / (depreciation[i] + ppe[i]))
+        sgai = safe_div(sgna[i] / sales[i], sgna[i-1] / sales[i-1])
+        lvgi = safe_div((long_term_debt[i] + current_liabilities[i]) / total_assets[i], (long_term_debt[i-1] + current_liabilities[i-1]) / total_assets[i-1])
+        tata = safe_div((net_income[i] - non_op_income[i] - cfo[i]), total_assets[i])
+
+        m_score = (
+            -4.84 +
+            0.92 * dsri +
+            0.528 * gmi +
+            0.404 * aqi +
+            0.892 * sgi +
+            0.115 * depi +
+            0.172 * sgai -
+            0.327 * lvgi +
+            4.679 * tata
+        )
+        results.append({
+            'Year': years[i],
+            'M-Score': round(m_score, 4),
+            'DSRI': round(dsri, 4),
+            'GMI': round(gmi, 4),
+            'AQI': round(aqi, 4),
+            'SGI': round(sgi, 4),
+            'DEPI': round(depi, 4),
+            'SGAI': round(sgai, 4),
+            'LVGI': round(lvgi, 4),
+            'TATA': round(tata, 4)
+        })
+
+    return results
+
