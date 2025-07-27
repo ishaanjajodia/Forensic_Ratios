e242ae4c10989ec9d75e1cf063eafc6124aea972
ishaanjajodia <153287021+ishaanjajodia@users.noreply.github.com>
2025-07-27 15:26:09 +0530

    Update ratios.py

--- ratios.py
+++ ratios.py
@@ -136,3 +136,133 @@ def calculate_beneish_m_score(data):
 
 
     return results
+
+
+
+
+
+
+# ratios.py
+
+def calculate_piotroski_f_score(annuals):
+    def safe_div(a, b):
+        return a / b if b else 0
+
+    years = []
+    net_income = []
+    cfo = []
+    total_assets = []
+    total_assets_beg = []
+    revenue = []
+    gross_profit = []
+    long_term_debt = []
+    current_assets = []
+    current_liabilities = []
+    shares_outstanding = []
+
+    for i, entry in enumerate(annuals):
+        years.append(entry['FiscalYear'])
+        inc = entry['stockFinancialMap']['INC']
+        net_income.append(float(next((v['value'] for v in inc if v['key'] == 'NetIncome'), 0)))
+        revenue.append(float(next((v['value'] for v in inc if v['key'] == 'TotalRevenue'), 0)))
+        gross_profit.append(float(next((v['value'] for v in inc if v['key'] == 'GrossProfit'), 0)))
+        cas = entry['stockFinancialMap']['CAS']
+        cfo.append(float(next((v['value'] for v in cas if v['key'] == 'CashfromOperatingActivities'), 0)))
+        bal = entry['stockFinancialMap']['BAL']
+        total_assets.append(float(next((v['value'] for v in bal if v['key'] == 'TotalAssets'), 0)))
+        long_term_debt.append(float(next((v['value'] for v in bal if v['key'] == 'LongTermDebt'), 0)))
+        current_assets.append(float(next((v['value'] for v in bal if v['key'] == 'TotalCurrentAssets'), 0)))
+        current_liabilities.append(float(next((v['value'] for v in bal if v['key'] == 'TotalCurrentLiabilities'), 0)))
+        shares_outstanding.append(float(next((v['value'] for v in bal if v['key'] == 'TotalCommonSharesOutstanding'), 0)))
+
+    # Total assets beginning of year
+    total_assets_beg = [total_assets[0]] + total_assets[:-1]
+
+    results = []
+    for i in range(len(years)):
+        fscore = 0
+        criteria = []
+
+        # 1. ROA > 0
+        roa = safe_div(net_income[i], total_assets_beg[i])
+        crit1 = roa > 0
+        fscore += int(crit1)
+        criteria.append(crit1)
+
+        # 2. CFO > 0
+        cfroa = safe_div(cfo[i], total_assets_beg[i])
+        crit2 = cfroa > 0
+        fscore += int(crit2)
+        criteria.append(crit2)
+
+        # 3. ROA increase
+        if i > 0:
+            roa_prev = safe_div(net_income[i-1], total_assets_beg[i-1])
+            crit3 = roa > roa_prev
+        else:
+            crit3 = False
+        fscore += int(crit3)
+        criteria.append(crit3)
+
+        # 4. Accruals: CFO > Net Income
+        crit4 = cfo[i] > net_income[i]
+        fscore += int(crit4)
+        criteria.append(crit4)
+
+        # 5. Leverage ↓
+        if i > 0:
+            avg_assets = (total_assets[i] + total_assets[i-1]) / 2
+            avg_assets_prev = (total_assets[i-1] + total_assets[i-2]) / 2 if i > 1 else avg_assets
+            gearing = safe_div(long_term_debt[i], avg_assets)
+            gearing_prev = safe_div(long_term_debt[i-1], avg_assets_prev)
+            crit5 = gearing < gearing_prev
+        else:
+            crit5 = False
+        fscore += int(crit5)
+        criteria.append(crit5)
+
+        # 6. Current Ratio ↑
+        if i > 0:
+            curr_ratio = safe_div(current_assets[i], current_liabilities[i])
+            curr_ratio_prev = safe_div(current_assets[i-1], current_liabilities[i-1])
+            crit6 = curr_ratio > curr_ratio_prev
+        else:
+            crit6 = False
+        fscore += int(crit6)
+        criteria.append(crit6)
+
+        # 7. No new shares
+        if i > 0:
+            crit7 = shares_outstanding[i] <= shares_outstanding[i-1]
+        else:
+            crit7 = True
+        fscore += int(crit7)
+        criteria.append(crit7)
+
+        # 8. Gross Margin ↑
+        if i > 0:
+            gm = safe_div(gross_profit[i], revenue[i])
+            gm_prev = safe_div(gross_profit[i-1], revenue[i-1])
+            crit8 = gm > gm_prev
+        else:
+            crit8 = False
+        fscore += int(crit8)
+        criteria.append(crit8)
+
+        # 9. Asset Turnover ↑
+        if i > 0:
+            at = safe_div(revenue[i], total_assets_beg[i])
+            at_prev = safe_div(revenue[i-1], total_assets_beg[i-1])
+            crit9 = at > at_prev
+        else:
+            crit9 = False
+        fscore += int(crit9)
+        criteria.append(crit9)
+
+        results.append({
+            'Year': years[i],
+            'F-Score': fscore,
+            'Criteria': criteria
+        })
+
+    return results
